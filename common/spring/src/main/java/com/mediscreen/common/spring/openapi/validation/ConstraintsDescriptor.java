8c01ee743cc1e7a5e5943b09fa9de7ab1d494d2b
np111 <oc.np111@protonmail.com>
2021-04-15 07:53:13 +0200

    Add open api constraints validation documentation

---
+++ common/spring/src/main/java/com/mediscreen/common/spring/openapi/validation/ConstraintsDescriptor.java
@@ -0,0 +1,195 @@
+package com.mediscreen.common.spring.openapi.validation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.AnnotatedParameterizedType;
+import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.validation.Constraint;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.Default;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.web.bind.annotation.ValueConstants.DEFAULT_NONE;
+
+@UtilityClass
+@Slf4j
+public class ConstraintsDescriptor {
+    public static List<Description> describeParameters(Method method) {
+        List<Description> descriptions = new ArrayList<>();
+        for (Parameter param : method.getParameters()) {
+            String name;
+            boolean required;
+
+            Annotation annotation;
+            if ((annotation = AnnotationUtils.getAnnotation(param, RequestBody.class)) != null) {
+                RequestBody requestBody = (RequestBody) annotation;
+                name = "body";
+                required = requestBody.required();
+            } else if ((annotation = AnnotationUtils.getAnnotation(param, PathVariable.class)) != null) {
+                PathVariable pathVariable = (PathVariable) annotation;
+                name = pathVariable.name();
+                required = pathVariable.required();
+            } else if ((annotation = AnnotationUtils.getAnnotation(param, RequestParam.class)) != null) {
+                RequestParam requestParam = (RequestParam) annotation;
+                name = requestParam.name();
+                required = requestParam.required() && DEFAULT_NONE.equals(requestParam.defaultValue());
+            } else {
+                continue;
+            }
+
+            Description description = new Description(name);
+            if (required) {
+                description.getConstraints().add(describe(NotNull.class, Collections.emptyMap()));
+            }
+            describe(description, param.getAnnotatedType(), param, new Class[0]);
+            if (!description.isEmpty()) {
+                descriptions.add(description);
+            }
+        }
+        return descriptions;
+    }
+
+    @SneakyThrows
+    private static void describe(Description dst, AnnotatedType type, AnnotatedElement annotations, Class<?>[] validationGroups) {
+        // describe own constraints
+        for (Annotation annotation : annotations.getAnnotations()) {
+            if (AnnotationUtils.getAnnotation(annotation.annotationType(), Constraint.class) != null) {
+                Method groupsMethod = MethodUtils.getMatchingMethod(annotation.annotationType(), "groups");
+                Class<?>[] constraintGroups = (Class<?>[]) groupsMethod.invoke(annotation);
+                if (isActive(constraintGroups, validationGroups)) {
+                    dst.getConstraints().add(describe(annotation));
+                }
+            }
+        }
+
+        // describe validated fields constraints
+        Class<?>[] fieldsValidationGroups = null;
+        Validated validated = AnnotationUtils.getAnnotation(annotations, Validated.class);
+        if (validated != null) {
+            fieldsValidationGroups = validated.value();
+        } else if (AnnotationUtils.getAnnotation(annotations, Valid.class) != null) {
+            fieldsValidationGroups = new Class[0];
+        }
+        if (fieldsValidationGroups != null) {
+            for (Field field : FieldUtils.getAllFields((Class<?>) type.getType())) {
+                Description description = new Description(field.getName());
+                describe(description, field.getAnnotatedType(), field, fieldsValidationGroups);
+                if (!description.isEmpty()) {
+                    dst.getFields().add(description);
+                }
+            }
+        }
+
+        // describe generic constraints
+        if (type instanceof AnnotatedParameterizedType) {
+            AnnotatedParameterizedType parameterizedType = (AnnotatedParameterizedType) type;
+
+            Class<?> clazz = (Class<?>) ((ParameterizedType) parameterizedType.getType()).getRawType();
+            List<String> argumentsNames = new ArrayList<>();
+            if (List.class.isAssignableFrom(clazz)) {
+                argumentsNames.add("entry");
+            } else if (Map.class.isAssignableFrom(clazz)) {
+                argumentsNames.add("key");
+                argumentsNames.add("value");
+            }
+
+            AnnotatedType[] arguments = parameterizedType.getAnnotatedActualTypeArguments();
+            for (int i = 0; i < arguments.length; i++) {
+                AnnotatedType argument = arguments[i];
+                Description description = new Description(
+                        "[" + (i < argumentsNames.size() ? argumentsNames.get(i) : Integer.toString(i + 1)) + "]");
+                describe(description, argument, argument, validationGroups);
+                if (!description.isEmpty()) {
+                    dst.getFields().add(description);
+                }
+            }
+        }
+    }
+
+    private static boolean isActive(Class<?>[] constraintGroups, Class<?>[] validationGroups) {
+        if (constraintGroups.length == 0) {
+            constraintGroups = new Class[]{Default.class};
+        }
+        if (validationGroups.length == 0) {
+            validationGroups = new Class[]{Default.class};
+        }
+        for (Class<?> validationGroup : validationGroups) {
+            for (Class<?> constraintGroup : constraintGroups) {
+                if (constraintGroup.isAssignableFrom(validationGroup)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static String describe(Annotation constraint) {
+        Map<String, Object> parameters = new HashMap<>();
+        for (Method method : constraint.annotationType().getMethods()) {
+            if (method.getDeclaringClass() != Annotation.class && method.canAccess(constraint)) {
+                String name = method.getName();
+                switch (name) {
+                    case "groups":
+                    case "message":
+                    case "payload":
+                        continue;
+                }
+                try {
+                    Object value = method.invoke(constraint);
+                    if (!Objects.equals(method.getDefaultValue(), value)) {
+                        parameters.put(name, value);
+                    }
+                } catch (IllegalAccessException | InvocationTargetException ex) {
+                    logger.warn("Exception retrieving annotation parameter:", ex);
+                }
+            }
+        }
+        return describe(constraint.annotationType(), parameters);
+    }
+
+    private static String describe(Class<? extends Annotation> constraintType, Map<String, Object> parameters) {
+        return constraintType.getSimpleName() + (parameters.isEmpty() ? ""
+                : '(' + parameters.entrySet().stream()
+                        .map(e -> e.getKey() + '=' + e.getValue())
+                        .collect(Collectors.joining(", "))
+                        + ')');
+    }
+
+    @RequiredArgsConstructor
+    @Data
+    public static class Description {
+        private final String name;
+        private final Set<String> constraints = new LinkedHashSet<>();
+        private final List<Description> fields = new ArrayList<>();
+
+        public boolean isEmpty() {
+            return constraints.isEmpty() && fields.isEmpty();
+        }
+    }
+}
---
+++ common/spring/src/main/java/com/mediscreen/common/spring/openapi/validation/ValidationCustomizer.java
@@ -0,0 +1,55 @@
+package com.mediscreen.common.spring.openapi.validation;
+
+import com.mediscreen.common.api.model.ApiError;
+import com.mediscreen.common.api.model.ApiErrorType;
+import com.mediscreen.common.spring.openapi.error.ApiErrorCustomizer;
+import io.swagger.v3.oas.models.Operation;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.method.HandlerMethod;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Component
+@Order(1)
+public class ValidationCustomizer implements OperationCustomizer {
+    private final ApiErrorCustomizer apiErrorCustomizer;
+
+    public Operation customize(Operation operation, HandlerMethod handlerMethod) {
+        if (handlerMethod.getMethod().getDeclaringClass().isAnnotationPresent(Validated.class)) {
+            List<ConstraintsDescriptor.Description> constraints = ConstraintsDescriptor.describeParameters(handlerMethod.getMethod());
+            if (!constraints.isEmpty()) {
+                ApiError apiError = ApiError.builder()
+                        .type(ApiErrorType.CLIENT)
+                        .code("VALIDATION_FAILED")
+                        .message("Validation failed: {{message}}")
+                        .metadata("parameter", "{{request parameter name}}")
+                        .metadata("constraint", "{{constraint class name}}")
+                        .metadata("{{constraint attribute}}", "{{value}}")
+                        .build();
+                StringBuilder description = new StringBuilder();
+                description.append("A request parameter validation failed:");
+                constraintsToString(description, "", constraints);
+                apiErrorCustomizer.addApiErrorResponse(operation, apiError, HttpStatus.BAD_REQUEST.value(), description.toString(), null);
+            }
+        }
+        return operation;
+    }
+
+    private void constraintsToString(StringBuilder sb, String prefix, List<ConstraintsDescriptor.Description> descriptions) {
+        for (ConstraintsDescriptor.Description description : descriptions) {
+            sb.append("\n").append(prefix).append("- *").append(description.getName()).append("*:");
+            int i = 0;
+            for (String constraint : description.getConstraints()) {
+                sb.append(++i == 1 ? " " : ", ");
+                sb.append(constraint);
+            }
+            constraintsToString(sb, prefix + "  ", description.getFields());
+        }
+    }
+}
